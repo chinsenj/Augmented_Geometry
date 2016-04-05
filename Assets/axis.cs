a18bf4addba21322fbc52cbe723cec6c7612f245
Jeremy <jeremychinsen@inside-186-228.wireless.utoronto.ca>
2016-04-05 19:18:25 -0400

    Added axes

---
+++ Assets/axis.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class axis : MonoBehaviour {
+
+	void Start () {
+		MeshGenerator parent = transform.parent.GetComponent<MeshGenerator>();
+		float centre = parent.resolution/2;
+		transform.localPosition = new Vector3(0.5f, 0.5f, 0.5f); 
+	}
+
+	void Update () {
+	
+	}
+}
