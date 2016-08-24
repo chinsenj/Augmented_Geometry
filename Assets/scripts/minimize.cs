77db19814fef7447a5a21df319300c62bb05b980
Jeremy Chinsen <jeremy@Jeremys-MacBook-Air.local>
2016-08-24 12:54:40 -0400

    Added UI and shader for mesh colour

---
+++ Assets/scripts/minimize.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class minimize : MonoBehaviour {
+
+	private bool minimized; 
+	public GameObject targetPanel;
+	public Button minButton;
+	public Text buttonText;
+
+	// Use this for initialization
+	void Start () {
+		minimized = false;
+		minButton.onClick.AddListener(()=>minimizePanel());
+	}
+
+	void minimizePanel(){
+		if (minimized == true) {
+			minimized = false;
+			buttonText.text = "-";
+			targetPanel.SetActive (true);
+		} 
+		else {
+			minimized = true;
+			buttonText.text = "+";
+			targetPanel.SetActive(false);
+		}
+			
+	}
+}
